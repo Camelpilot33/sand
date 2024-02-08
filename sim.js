42062fb63cbde49a54a5a9fa606bfd8a5ec8670c
Samuel Sastry <anonymouscamul@gmail.com>
2024-02-08 07:42:51 -0800

    go

--- sim.js
+++ sim.js
@@ -12,12 +12,11 @@ function set(x, y, rgb = [0, 0, 0]) {
 }
 function draw() {
     clear()
-    for (x = 0; x <= canvas.width / zoom - 1; x++) {
-        for (y = 0; y <= canvas.height / zoom - 1; y++) {
-            set(x, canvas.height / zoom-y-1, world[x][y].color)
-            // set(x, y, [Math.random(),Math.random(),Math.random()])
-        }
+    for (x = 0; x <= canvas.width / zoom - 1; x++) for (y = 0; y <= canvas.height / zoom - 1; y++) {
+        set(x, canvas.height / zoom - y - 1, world[x][y].color)
+        // set(x, y, [Math.random(),Math.random(),Math.random()])
     }
+
 }
 class Cell {
     constructor(state = "air") {
@@ -32,10 +31,29 @@ class Cell {
                 break
         }
     }
+    attempt(ox,oy,x, y) {
+        if (x < 0 || x > canvas.width / zoom - 1 || y < 0 || y > canvas.height / zoom - 1) return false
+        if (world[x][y].state == "air") {
+            world[x][y] = this
+            world[ox][oy] = new Cell("air")
+            return true
+        }
+        return false
+    }
+}
+function frame() {
+    for (x = 0; x <= canvas.width / zoom - 1; x++) for (y = 0; y <= canvas.height / zoom - 1; y++) {
+        if (world[x][y].state == "sand") {
+           (world[x][y].attempt(x,y,x, y - 1) )
+        }
+    }
+    draw()
 }
 
 let world = new Array(canvas.width / zoom)
     .fill(0).map(e => new Array(canvas.height / zoom)
-    .fill(0).map(w=>new Cell("air")))
+        .fill(0).map(w => new Cell("air")))
 world[1][3] = new Cell("sand")
-draw(world)
+setInterval(frame, 1000 / 5)
+setInterval(function(){world[1][9] = new Cell("sand")}, 1000)
+// frame()
